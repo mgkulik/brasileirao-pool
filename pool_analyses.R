6b42dc4dbdb40c7b08ac1e4fe23291391f03ce10
Mariane Goncalves <mari.ti@gmail.com>
2021-05-24 11:43:56 -0300

    The execution is too slow. Trying to remove images from the page to make it faster.

--- pool_analyses.R
+++ pool_analyses.R
@@ -1,6 +1,8 @@
 library(pacman)
 p_load(RColorBrewer, ggplot2, ggrepel, dplyr, openxlsx, here, ggpubr)
 
+pall <- colorRampPalette(brewer.pal(8, "Set2"))(10)
+
 #brasileirao2021 <- openxlsx::read.xlsx('brasileirao_2021.xlsx', sheet = 'Sheet1')
 brasileirao <- openxlsx::read.xlsx('brasileirao_last5years.xlsx', sheet = 'matches')
 geo <- openxlsx::read.xlsx('brasileirao_last5years.xlsx', sheet = 'region')
@@ -23,11 +25,24 @@ brasileirao$draw_score_cat <- factor(ifelse(brasileirao$type_win == 'Draw',
                                                    ifelse(brasileirao$away_score==1, '1', 
                                                           ifelse(brasileirao$away_score==2, '2',
                                                                  ifelse(brasileirao$away_score==3, '3', '4-6')))), NA))
-brasileirao$matchday <- rep_round <- factor(rep(1:10, nrow(brasileirao)/10))
+brasileirao$matchday <- rep_round <- factor(rep(1:38, each=10))
 
 result_table <- openxlsx::read.xlsx('brasileirao_last5years.xlsx', sheet = 'table')
 result_table$year <- factor(result_table$year)
 brasileirao <- brasileirao%>%left_join(result_table, by=c("home_team"="team", "year"))
+brasileirao <- brasileirao%>%left_join(result_table, by=c("away_team"="team", "year"), suffix=c("_home", "_away"))
+
+table_acc <- result_table%>%
+  filter(position<=5)%>%
+  group_by(team)%>%tally()%>%
+  rename("top_pos_5y"=n)
+
+table2020 <- result_table%>%
+  filter(year=="2020")%>%
+  left_join(table_acc, by="team")
+#write.csv(table2020, "table_2020.csv", fileEncoding="latin1", row.names=FALSE)
+
+##### QUESTION 1 - Which was the distribution of the Home, Away Widns and Draws in the past 5 years? #####
 
 gen_year_win <- ggplot(brasileirao, aes(x=year, fill=type_win)) +
   geom_bar(position = position_dodge2(reverse=TRUE)) +
@@ -50,6 +65,8 @@ gen_year_win_perc <- ggplot(brasileirao, aes(x=year, fill=type_win)) +
 composite_type_win <- ggarrange(gen_year_win, gen_year_win_perc, widths=c(2,1))
 composite_type_win
 
+##### QUESTION 2 - Which were the distribution of the scores in the past 5 years? #####
+
 gen_year_score <- ggplot(brasileirao, aes(x=score, fill=year)) +
   geom_bar(position = position_dodge2(reverse=TRUE)) +
   scale_fill_brewer(palette = "Set2") +
@@ -67,6 +84,9 @@ gen_year_score_perc <- ggplot(brasileirao, aes(x=score, fill=year)) +
   labs(title="% Score by Year", x="Score", y="Counts", fill="Year")+
   theme_bw()
 gen_year_score_perc
+
+##### QUESTION 3 - Which % of the home, away wins and draws for the score by year? #####
+# We do expect some tendencies of scores 0-X, 1-X, X-1, and 2-X, but how often they happend?
 
 gen_home_cat_perc <- brasileirao%>%
   filter(type_win=='Home Win')%>%
@@ -136,22 +156,24 @@ gen_draw_cat <- brasileirao%>%
 composite_score_cat <- ggarrange(gen_home_cat, gen_away_cat, gen_draw_cat, nrow=1)
 ggplot2::ggsave("composite_score_cat.png", composite_score_cat, units = "in", width = 20, height = 8, dpi = 300)
 
-pall <- colorRampPalette(palette('Set2'))(10)
 
+##### QUESTION 4 - There's a tendency for the teams with better scores to relax in the end of the championship? #####
+
+# All teams
 gen_home_md_win <- brasileirao%>%
   filter(type_win=='Home Win')%>%
-  ggplot(aes(x=year, fill=matchday)) +
+  ggplot(aes(x=matchday, fill=year)) +
   geom_bar(position = position_dodge2(reverse=TRUE)) +
-  scale_fill_brewer(palette = pall) +
+  scale_fill_brewer(palette = 'Set2') +
   geom_text(stat='count', aes(label = after_stat(count)), position = position_dodge2(width = .9, reverse=TRUE), vjust = -0.25, size = 5) +
   labs(title="Home Win by Year and Matchday", x="Year", y="Counts", fill="Matchday")+
   theme_bw()
 
 gen_home_md_win_perc <- brasileirao%>%
   filter(type_win=='Home Win')%>%
-  ggplot(aes(x=year, fill=matchday)) +
+  ggplot(aes(x=matchday, fill=year)) +
   geom_bar(stat='count', position = "fill") +
-  scale_fill_brewer(palette = pall) +
+  scale_fill_brewer(palette = 'Set2') +
   geom_text(aes(label = paste0(round((..count../tapply(..count.., ..x.. ,sum)[..x..])*100, 2),'%')),
             position = position_fill(vjust = 0.5), stat = "count", size = 4) +
   scale_y_continuous(labels = scales::percent) + 
@@ -161,8 +183,45 @@ gen_home_md_win_perc <- brasileirao%>%
 composite_home_md_win <- ggarrange(gen_home_md_win, gen_home_md_win_perc, nrow=1)
 composite_home_md_win
 
+# Top 5
+gen_top5_match <- brasileirao%>%
+  filter(position_home<=5 | position_away<=5)%>%
+  ggplot(aes(x=matchday, fill=year)) +
+  geom_bar(position = position_dodge2(reverse=TRUE)) +
+  scale_fill_brewer(palette = 'Set2') +
+  geom_text(stat='count', aes(label = after_stat(count)), position = position_dodge2(width = .9, reverse=TRUE), vjust = -0.25, size = 5) +
+  labs(title="Home Win by Year and Matchday", x="Year", y="Counts", fill="Matchday")+
+  theme_bw()
+
+gen_top5_match_perc <- brasileirao%>%
+  filter(position_home<=5 | position_away<=5)%>%
+  ggplot(aes(x=matchday, fill=year)) +
+  geom_bar(stat='count', position = "fill") +
+  scale_fill_brewer(palette = 'Set2') +
+  geom_text(aes(label = paste0(round((..count../tapply(..count.., ..x.. ,sum)[..x..])*100, 2),'%')),
+            position = position_fill(vjust = 0.5), stat = "count", size = 4) +
+  scale_y_continuous(labels = scales::percent) + 
+  labs(title="% Home Win by Year and Matchday", x="Year", y="Counts", fill="Matchday")+
+  theme_bw()
+
+composite_top5_match <- ggarrange(gen_top5_match, gen_top5_match_perc, nrow=1)
+composite_top5_match
+
+##### OTHER #####
+pall <- colorRampPalette(brewer.pal(8, "Set2"))(12)
+
+gen_team <- result_table%>%
+  filter(position<=5)%>%
+  ggplot(aes(x=team, fill=team)) +
+  geom_bar(position = position_dodge2(reverse=TRUE)) +
+  scale_fill_manual(values = pall) +
+  geom_text(stat='count', aes(label = after_stat(count)), position = position_dodge2(width = .9, reverse=TRUE), vjust = -0.25, size = 5) +
+  labs(title="Type of Win by Year", x="Year", y="Counts", fill="Type of Win")+
+  theme_bw()
+gen_team
+
 gen_team <- brasileirao%>%
-  filter(status=="top 4", type_win=="Home Win")%>%
+  filter(status_home=="top 4", type_win=="Home Win")%>%
   ggplot(aes(x=matchday, fill=home_team)) +
   geom_bar(position = position_dodge2(reverse=TRUE)) +
   scale_fill_brewer(palette = "Set2") +
@@ -190,6 +249,7 @@ gen_team_matches <- result_table%>%
   theme_bw()
 gen_team_matches
 
+
 gen_fla_score <- brasileirao%>%
   filter(home_team=="FLAMENGO")%>%
   ggplot(aes(x=score, fill=year)) +
@@ -200,6 +260,68 @@ gen_fla_score <- brasileirao%>%
   labs(title="Flamengo Score", x="Score", y="Counts", fill="Year")+
   theme_bw()
 gen_fla_score
+
+tb_top5 <-brasileirao%>%
+  filter(year=="2020", position_home<=5)%>%
+  group_by(type_win)%>%
+  tally()
+
+tb_other <-brasileirao%>%
+  filter(year=="2020", position_home>5)%>%
+  group_by(type_win)%>%
+  tally()
+
+top5_score <- brasileirao%>%
+  filter(year=="2020", position_home<=5)%>%
+  ggplot(aes(x=type_win, fill=type_win)) +
+  geom_bar(position = position_dodge2(reverse=TRUE)) +
+  scale_fill_manual(values = pall) +
+  geom_text(stat='count', aes(label = after_stat(count)), position = position_dodge2(width = .9, reverse=TRUE), vjust = -0.25, size = 5) +
+  labs(title="Top 5 Scores by Matchday", x="Score", y="Counts", fill="Matchday")+
+  theme_bw()
+
+las15_score <- brasileirao%>%
+  filter(year=="2020", position_home>5)%>%
+  ggplot(aes(x=type_win, fill=type_win)) +
+  geom_bar(position = position_dodge2(reverse=TRUE)) +
+  scale_fill_manual(values = pall) +
+  geom_text(stat='count', aes(label = after_stat(count)), position = position_dodge2(width = .9, reverse=TRUE), vjust = -0.25, size = 5) +
+  labs(title="Top 5 Scores by Matchday", x="Score", y="Counts", fill="Matchday")+
+  theme_bw()
+
+
+las7_score <- brasileirao%>%
+  filter(year=="2020", position_home>13)%>%
+  ggplot(aes(x=type_win, fill=type_win)) +
+  geom_bar(position = position_dodge2(reverse=TRUE)) +
+  scale_fill_manual(values = pall) +
+  geom_text(stat='count', aes(label = after_stat(count)), position = position_dodge2(width = .9, reverse=TRUE), vjust = -0.25, size = 5) +
+  labs(title="Top 5 Scores by Matchday", x="Score", y="Counts", fill="Matchday")+
+  theme_bw()
+
+composite_top_last <- ggarrange(top5_score, las15_score, las7_score, nrow=1)
+composite_top_last
+
+top5_low11_score <- brasileirao%>%
+  filter(year=="2020", (position_home<=5 & position_away>=11))%>%
+  ggplot(aes(x=type_win, fill=type_win)) +
+  geom_bar(position = position_dodge2(reverse=TRUE)) +
+  scale_fill_manual(values = pall) +
+  geom_text(stat='count', aes(label = after_stat(count)), position = position_dodge2(width = .9, reverse=TRUE), vjust = -0.25, size = 5) +
+  labs(title="5 Top x Lox Scores by Away Team", x="Score", y="Counts", fill="Matchday")+
+  theme_bw()
+
+low5_top11_score <- brasileirao%>%
+  filter(year=="2020", (position_home>=16 & position_away<=10))%>%
+  ggplot(aes(x=type_win, fill=type_win)) +
+  geom_bar(position = position_dodge2(reverse=TRUE)) +
+  scale_fill_manual(values = pall) +
+  geom_text(stat='count', aes(label = after_stat(count)), position = position_dodge2(width = .9, reverse=TRUE), vjust = -0.25, size = 5) +
+  labs(title="5 Top x Lox Scores by Away Team", x="Score", y="Counts", fill="Matchday")+
+  theme_bw()
+
+composite_top5 <- ggarrange(top5_low11_score, low5_top11_score, nrow=1)
+composite_top5
 
 tb_fla_score <- result_table%>%
   filter(team=="FLAMENGO")
--- pool_automation.py
+++ pool_automation.py
@@ -11,6 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import numpy as np
 import pandas as pd
 import random
+import time
 
 USERNAME = input("Inform the username of the Wordpress based website: ")
 PASSWORD = input("Inform the password of the Wordpress based website: ")
@@ -18,9 +19,13 @@ URL = input("Inform the URL of the targed website: ")
 R_SEED = input("Give an integer number with 5 positions: ")
 N_MATCHES = 38
 path_year = 'brasileirao_2021.csv'
+path_other = 'table_2020.csv'
 
 def access_page():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-datadir=/home/datascience/.config/google-chrome/Profile 1");
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get(URL)
     user_input = driver.find_element_by_id('user_login')
     user_input.send_keys(USERNAME)
@@ -32,34 +37,79 @@ def access_page():
     login_button.click()
     
     time.sleep(5)
+    return driver
+    
+# Mock values:
+# df_guess = pd.DataFrame(np.transpose(np.vstack((np.ones((1,len(df2021))),np.zeros((1,len(df2021)))))), columns=['home', 'away'])
+def set_field_values(driver):
+    df_guess = pd.DataFrame(np.transpose(np.vstack((np.ones((1,len(df2021))),np.zeros((1,len(df2021)))))), columns=['home', 'away'])
+    for i in range(1,384):
+        if i!=70:
+            home_input = driver.find_element_by_name('_home_'+str(i))
+            home_input.send_keys(str(int(df_guess.iloc[i-1,0])))
+            away_input = driver.find_element_by_name('_away_'+str(i))
+            away_input.send_keys(str(int(df_guess.iloc[i-1,1])))
+        
     
 def basic_checks():
     assert len(str(R_SEED)) == 5, "Provide a whole number with size 5!"
-    assert isinstance(R_SEED, int), "Provide a whole number with size 5!"
     assert len(str(USERNAME)) > 0, "Provide the username."
     assert len(str(PASSWORD)) > 0, "Provide the password."
     assert len(str(URL)) > 0, "Provide the URL with http/https."
     
 
-def mark_team_victories(df2021, team, p_win, p_home_win, p_away_win):
+def mark_team_victories(df2021, team, p_win, p_home_win):
+    '''Define which matches of the top teams will be home or away wins. This
+    ensure these teams will surelly win some matches, reducing the selection
+    by chance.'''
     n_win_home = round(N_MATCHES*p_win*p_home_win)
-    n_win_away = round(N_MATCHES*p_win*p_away_win)
+    n_win_away = round(N_MATCHES*p_win*(1-p_home_win))
     ids_match_home = list(df2021.loc[df2021['home_team']==team].index)
     ids_match_away = list(df2021.loc[df2021['away_team']==team].index)
     random.seed(R_SEED)
     rand_ids_home = random.sample(ids_match_home, k=n_win_home)
-    print(rand_ids_home)
     random.seed(R_SEED)
     rand_ids_away = random.sample(ids_match_away, k=n_win_away)
-    print(rand_ids_away)
     df2021.at[rand_ids_home, 'win_home'] = 1
     df2021['win_home'] = df2021['win_home'].fillna(0)
     df2021.at[rand_ids_away, 'win_away'] = 1
     df2021['win_away'] = df2021['win_away'].fillna(0)
     return df2021
 
+def merge_last_main(df2021, dftable2020):
+    dfpos = dftable2020[['team', 'position']]
+    df2021 = pd.merge(df2021, dfpos, how='left', left_on="home_team", right_on="team", copy=False)
+    df2021 = pd.merge(df2021, dfpos, how='left', left_on="away_team", right_on="team", suffixes=('_home', '_away'), copy=False)
+    df2021 = df2021.drop(['team_home', "team_away"], axis=1)
+    pos_new = int(max(df2021['position_home'])+1)
+    df2021['position_home'] = df2021['position_home'].fillna(pos_new)
+    df2021['position_away'] = df2021['position_away'].fillna(pos_new)
+    return df2021
+
+def scores_win_home(df2021, perc):
+    sel_win = sum(df2021['win_home'])
+    to_sel = int(round(N_MATCHES*10*perc, 0)-sel_win)
+    ids_match_home = list(df2021.loc[df2021['win_home']==0].index)
+    random.seed(R_SEED)
+    rand_ids_home = random.sample(ids_match_home, k=to_sel)
+    
+
+#def main():
 if __name__ == '__main__':
+    R_SEED = int(R_SEED)
     basic_checks()
     df2021 = pd.read_csv(path_year, encoding='latin1')
-    df2021 = mark_team_victories(df2021, "FLAMENGO", .5, .8, .2)
+    dftable2020 = pd.read_csv(path_other, encoding='latin1')
+    df2021 = merge_last_main(df2021, dftable2020)
+    
+    top = list(dftable2020.loc[dftable2020['top_pos_5y'] >=3, 'team'].values)
+    top_perc = [.55,.52,.5,.48]
+    top_home = [.8,.75,.7,.65]
+    
+    for a,b,c in zip(top, top_perc, top_home):
+        df2021 = mark_team_victories(df2021, a, b, c)
+    
+    driver = access_page()
+    set_field_values(driver)
+    
     
